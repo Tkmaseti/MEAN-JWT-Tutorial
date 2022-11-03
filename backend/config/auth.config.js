b8802f008a73ff6ad4afaa6782e2499fb06fe0df
Tsepang Maseti <maseti.tsepang@gmail.com>
2022-11-03 09:08:23 +0200

    first commit

---
+++ backend/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "coding-is-fun"
+};
---
+++ frontend/JWT-Fontend/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { BoardAdminComponent } from './components/board-admin/board-admin.component';
+import { BoardModeratorComponent } from './components/board-moderator/board-moderator.component';
+import { BoardUserComponent } from './components/board-user/board-user.component';
+import { authInterceptorProviders } from'./_helpers/auth.interceptor';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ProfileComponent,
+    BoardAdminComponent,
+    BoardModeratorComponent,
+    BoardUserComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+  ],
+  providers: [authInterceptorProviders],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
